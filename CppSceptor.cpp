d13a525d654c8841fd3f6c71734666b1b9351f0c
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-08-28 01:45:46 -0700

    getting further into cmake tutorial

--- CppSceptor.cpp
+++ CppSceptor.cpp
@@ -1,17 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include "CppSceptor.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    // Are there enough arguments?
-    if(argc < 2) {
-        fprintf(stdout, "Usage: %s number\n", argv[0]);
-        return 1;
-    }
+int main(int argc, char **argv) {
+  // Are there enough arguments?
+  if (argc < 2) {
+    fprintf(stdout, "%s Version %d.%d\n", argv[0], CppSceptor_VERSION_MAJOR,
+            CppSceptor_VERSION_MINOR);
+    fprintf(stdout, "Usage: %s number\n", argv[0]);
+    return 1;
+  }
 
-    double inputValue = atof(argv[1]);
-    double outputValue = sqrt(inputValue);
+  double inputValue = atof(argv[1]);
+  double outputValue = sqrt(inputValue);
 
-    fprintf(stdout, "The square root of %g is %g\n", inputValue, outputValue);
-    return 0;
+  fprintf(stdout, "The square root of %g is %g\n", inputValue, outputValue);
+  return 0;
 }
---
+++ CppSceptor.h
@@ -0,0 +1,3 @@
+// Some configured options from the build.
+#define CppSceptor_VERSION_MAJOR 1
+#define CppSceptor_VERSION_MINOR 1
